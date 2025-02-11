4cff7d187eceb07be7b138f5613537f9ac4e60da
drpeterparsons <github@pjparsons.com>
2025-02-11 15:45:54 +0000

    Update mongobackup_to_s3.sh

--- mongobackup_to_s3.sh
+++ mongobackup_to_s3.sh
@@ -2,3 +2,4 @@
 BACKUPFILE="mongodb_backup_$(date +"%Y%m%d_%H%M%S").out"
 mongodump --out /tmp/${BACKUPFILE}
 aws s3 cp /tmp/{BACKUPFILE} s3://ppwizdemo/{BACKUPFILE} --recursive
+rm -rf /tmp/${BACKUPFILE}
