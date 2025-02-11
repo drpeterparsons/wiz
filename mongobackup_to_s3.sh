f60b835b1eb504b6570c405559c0923c99fb297d
drpeterparsons <github@pjparsons.com>
2025-02-11 15:18:45 +0000

    Update mongobackup_to_s3.sh

--- mongobackup_to_s3.sh
+++ mongobackup_to_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BACKUPFILE="mongodb_backup_$(date +"%Y%m%d_%H%M%S").out"
-mongodump --uri="mongodb://admin:password@localhost:27017/mydb" --out /tmp/${BACKUPFILE}
+mongodump --out /tmp/${BACKUPFILE}
 aws s3 cp /tmp/{BACKUPFILE} s3://your-bucket-name/
