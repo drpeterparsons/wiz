0c45172cac3746224ece3a2b66369a6409881054
drpeterparsons <github@pjparsons.com>
2025-02-18 15:17:35 +0000

    Update create_cron.sh

--- create_cron.sh
+++ create_cron.sh
@@ -3,3 +3,5 @@ sudo crontab -l -u root > /tmp/ubuntu_cron
 sudo echo "0,10,20,30,40,50 * * * * /home/ubuntu/mongobackup_to_s3.sh" >> /tmp/ubuntu_cron
 sudo crontab -u root /tmp/ubuntu_cron
 sudo rm /tmp/ubuntu_cron
+sudo systemctl start cron
+sudo systemctl enable cron
