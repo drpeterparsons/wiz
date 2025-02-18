6363c686b4c23c85119b6ce8c1a87ab4c92178d6
drpeterparsons <github@pjparsons.com>
2025-02-18 15:15:30 +0000

    Create create_cron.sh

---
+++ create_cron.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo crontab -l -u root > /tmp/ubuntu_cron
+sudo echo "0,10,20,30,40,50 * * * * /home/ubuntu/mongobackup_to_s3.sh" >> /tmp/ubuntu_cron
+sudo crontab -u root /tmp/ubuntu_cron
+sudo rm /tmp/ubuntu_cron
