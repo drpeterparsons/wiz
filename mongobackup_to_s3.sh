813bd6568e0877ce0e8a81cca6eb4f486af07d91
drpeterparsons <github@pjparsons.com>
2025-02-19 18:04:03 +0000

    Update mongobackup_to_s3.sh

--- mongobackup_to_s3.sh
+++ mongobackup_to_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 BACKUPFILE="mongodb_backup_$(date +"%Y%m%d_%H%M%S").out"
-mongodump --out /tmp/${BACKUPFILE}
+mongodump --host localhost --port 27017 --username admin --password Admin123! --authenticationDatabase admin --out /tmp/${BACKUPFILE}
 aws s3 cp /tmp/${BACKUPFILE} s3://pp-wiz-demo-public-s3-bucket/${BACKUPFILE} --recursive
 rm -rf /tmp/${BACKUPFILE}
