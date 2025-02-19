15b24e33ddbb4481edc4aad72a97d7ee95427488
drpeterparsons <github@pjparsons.com>
2025-02-19 18:11:37 +0000

    Update AddMongoAdminUser.js

--- AddMongoAdminUser.js
+++ AddMongoAdminUser.js
@@ -1,5 +1,5 @@
 db.createUser({
   user: "admin",               // Username for the admin user
-  pwd: "your-password",        // Strong password for the admin user
+  pwd: "Admin123!",        // Strong password for the admin user
   roles: [{ role: "root", db: "admin" }]  // Assign the 'root' role to this user
 })
